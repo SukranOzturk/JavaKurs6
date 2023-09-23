c9acf9da53e4a9ca814345a549e1ef85514af5af
Şükran Öztürk <sukranozturk234@gmail.com>
2023-09-23 18:55:22 +0300

    Java

---
+++ src/Gun09/_07_JavaIf.java
@@ -0,0 +1,26 @@
+package Gun09;
+
+import java.util.Scanner;
+
+public class _07_JavaIf {
+    public static void main(String[] args) {
+        // Girilen bir cümledeki küçük veya büyük a harfinin olup olmadığı yazdırınız
+        // VAR ve YOK şeklinde
+
+//        hava  -> var desin
+//        HAVA  -> var desin
+
+        Scanner oku= new Scanner(System.in);
+        System.out.print("Cümle giriniz=");
+        String cumle=oku.nextLine();
+
+        cumle=cumle.toLowerCase(); // bütün büyük harfler küçük oldu
+
+        if (cumle.contains("a"))
+            System.out.println("VAR");
+
+        if (!cumle.contains("a"))
+            System.out.println("YOK");
+
+    }
+}
